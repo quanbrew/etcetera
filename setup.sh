85cde57aa37a970c715af9760e1b697c4389b540
Quan Brew <t@ioover.net>
2017-10-14 02:39:20 +0800

    Simplify script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-cd $DIR
+cd $(dirname $0)
 git pull
 git submodule init
 git submodule update
-$DIR/link.sh
+ln -sf .vimrc $HOME/.vimrc
+ln -sf .tmux.conf.local $HOME/.tmux.conf.local
+ln -sf .tmux/.tmux.conf $HOME/.tmux.conf
+
