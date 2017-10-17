fd4d9240c1046f481028cbb01f4cf64b0dba114f
酿泉 <tioover@gmail.com>
2017-10-18 00:30:23 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ git pull
 git submodule init
 git submodule update
 ln -sf .vimrc $NOW/.vimrc
-ln -sf .tmux.conf.local $NOW/.tmux.conf.local
-ln -sf .tmux/.tmux.conf $NOW/.tmux/.tmux.conf
+ln -sf $NOW/.tmux.conf.local $HOME/.tmux.conf.local
+ln -sf $NOW/.tmux/.tmux.conf $HOME/.tmux/.tmux.conf
 
